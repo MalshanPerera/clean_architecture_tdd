1236cc47af1d22867479365e1473a1f0127363c6
MalshanP <malshan@gapstars.net>
2021-12-03 15:35:16 +0530

    Add Flavors to IOS and Android

---
+++ lib/main_dev.dart
@@ -0,0 +1,19 @@
+import 'package:clean_architecture_tdd/injection_container.dart' as di;
+import 'package:flutter/material.dart';
+
+import 'core/helper/app_enum.dart';
+import 'flavor_config.dart';
+import 'head.dart';
+
+void main() async {
+  FlavorConfig(
+    flavor: Flavor.DEV,
+    values: FlavorValues(
+      appName: "[Dev] Clean Architecture",
+      baseUrl: 'http://ec2-52-28-136-161.eu-central-1.compute.amazonaws.com:3000',
+    ),
+  );
+
+  di.init();
+  runApp(const Head());
+}
